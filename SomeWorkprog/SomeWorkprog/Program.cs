99baf2ec45d3aaf39d441eb912f080c1bc45f54c
exploit-crypto <62612694+exploit-crypto@users.noreply.github.com>
2020-03-24 20:27:59 +0300

    adasd

---
+++ SomeWorkprog/SomeWorkprog/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeWorkprog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter word:");
+            string str = Console.ReadLine();
+            Console.WriteLine("Date added!");
+        }
+    }
+}
